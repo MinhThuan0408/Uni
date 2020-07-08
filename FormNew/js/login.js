d3f1b2f9843f4c7d1dffe003471be94e6c4e290a
DESKTOP-GL1I4K1\Minerva-PC04 <minhthuansh@gmail.com>
2020-07-08 11:59:59 +0700

    loginnew complete

--- FormNew/js/login.js
+++ FormNew/js/login.js
@@ -2,6 +2,6 @@ $(document).on('click', '.toggle-password', function() {
     
     $(this).toggleClass("la-eye la-eye-slash");
     
-    var input = $("#pass_log_id");
+    var input = $(".loginBlock__content__inputPWBox");
     input.attr('type') === 'password' ? input.attr('type','text') : input.attr('type','password')
 });
