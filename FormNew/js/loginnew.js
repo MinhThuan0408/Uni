d5cb32ade0259508f2343a03355c8dfd4ecd224b
DESKTOP-GL1I4K1\Minerva-PC04 <minhthuansh@gmail.com>
2020-07-21 16:47:14 +0700

    del

--- FormNew/js/loginnew.js
+++
@@ -1,8 +0,0 @@
-$(document).on('click', '.toggle-password', function() {
-    
-    $(this).toggleClass("la-eye la-eye-slash");
-    
-    var input = $(".loginBlock__inputPWBox");
-    input.attr('type') === 'password' ? input.attr('type','text') : input.attr('type','password')
-});
-
--- LoginForm/js/login.js
+++
@@ -1,7 +0,0 @@
-$(document).on('click', '.toggle-password', function() {
-    
-    $(this).toggleClass("la-eye la-eye-slash");
-    
-    var input = $(".login__input");
-    input.attr('type') === 'password' ? input.attr('type','text') : input.attr('type','password')
-});
