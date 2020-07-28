3adae984a378ef2363ed1b28540ab30fa46d1f89
DESKTOP-GL1I4K1\Minerva-PC04 <minhthuansh@gmail.com>
2020-07-28 17:36:09 +0700

    ten class + responsive

--- AdminTemplate/js/main.js
+++ AdminTemplate/js/main.js
@@ -1,9 +1,9 @@
 //click show left_nav trên pc
 $(document).ready(function () {
-    $(".menu_show").click(function () {
+    $(".menuSidebar").click(function () {
         $('body').toggleClass('sidebar-expand')
         //$('.left_nav').toggleClass('show-sidebar');
-       
+
     });
 });
 
@@ -11,10 +11,10 @@ $(document).ready(function () {
 
 //list item in left_nav
 $(document).ready(function () {
-    $(".clickDrop").click(function() {
-        $(this).parent().addClass('active').find('.left_nav--biglist').slideToggle();
-        $(".clickDrop").not(this).parent().removeClass('active').find('.left_nav--biglist').slideUp();
-      });
+    $(".clickDrop").click(function () {
+        $(this).parent().addClass('active').find('.left_nav--big_list').slideToggle();
+        $(".clickDrop").not(this).parent().removeClass('active').find('.left_nav--big_list').slideUp();
+    });
 });
 
 
@@ -26,3 +26,33 @@ $(document).ready(function () {
     });
 });
 
+
+//show expand màn hình 991
+var windowsize = $(window).width();
+
+$(window).resize(function () {
+    windowsize = $(window).width();
+    if (windowsize >= 992) {
+        //Nếu màn hình nhỏ or bằng 992px thì xóa class sidebar-expand
+        $('body').removeClass('sidebar-expand')
+    }
+    if (windowsize <= 991) {
+        //Nếu màn hình nhỏ or bằng 991px thì add class sidebar-expand
+        $('body').addClass('sidebar-expand')
+    }
+    if (windowsize <= 767) {
+        //Nếu màn hình nhỏ or bằng 767px thì xóa class sidebar-expand
+        $('body').removeClass('sidebar-expand')
+    }
+});
+
+
+///hover expand
+// $(document).ready(function () {
+//     $(".left_nav").hover(function () {
+//         if ($("body").hasClass('sidebar-expand')) {
+//             $("body").toggle('sidebar-expand')
+//             $('.left_nav').css('position', 'absolute');
+//         }
+//     })
+// });
