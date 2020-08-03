90a62df82d0d56f32daa769c5f939e6d663c1d13
DESKTOP-GL1I4K1\Minerva-PC04 <minhthuansh@gmail.com>
2020-08-03 17:42:06 +0700

    silder + chart

--- AdminTemplate/js/main.js
+++ AdminTemplate/js/main.js
@@ -1,16 +1,16 @@
 //click show left_nav trên pc
 $(document).ready(function () {
     $(".menuSidebar").click(function () {
-        $('body').toggleClass('sidebar-expand')
+        $('body').toggleClass('sidebar_expand')
         $('body').toggleClass('hideDarkLogo')
     });
 });
 
-//css class activee (background,color...) + active big-list
+//css class custome_active (background,color...) + active big-list
 $(document).ready(function () {
     $('.clickDrop').click(function () {
-        $(".clickDrop").removeClass('activee').not(this).parent().removeClass('active').find('.left_nav--big_list').slideUp();
-        $(this).addClass('activee').parent().addClass('active').find('.left_nav--big_list').slideToggle();
+        $(".clickDrop").removeClass('custome_active').not(this).parent().removeClass('active').find('.left_nav--big_list').slideUp();
+        $(this).addClass('custome_active').parent().addClass('active').find('.left_nav--big_list').slideToggle();
     });
 });
 
@@ -28,29 +28,29 @@ var windowsize = $(window).width();
 $(window).resize(function () {
     windowsize = $(window).width();
     if (windowsize >= 992) {
-        //Nếu màn hình nhỏ or bằng 992px thì xóa class sidebar-expand
-        $('body').removeClass('sidebar-expand')
+        //Nếu màn hình nhỏ or bằng 992px thì xóa class sidebar_expand
+        $('body').removeClass('sidebar_expand')
     }
     if (windowsize <= 991) {
-        //Nếu màn hình nhỏ or bằng 991px thì add class sidebar-expand
-        $('body').addClass('sidebar-expand')
+        //Nếu màn hình nhỏ or bằng 991px thì add class sidebar_expand
+        $('body').addClass('sidebar_expand')
     }
     if (windowsize <= 767) {
-        //Nếu màn hình nhỏ or bằng 767px thì xóa class sidebar-expand
-        $('body').removeClass('sidebar-expand')
+        //Nếu màn hình nhỏ or bằng 767px thì xóa class sidebar_expand
+        $('body').removeClass('sidebar_expand')
     }
 });
 //hover dung JQuery
 //hover expand
 // $(document).ready(function () {
 //     $(".left_nav").mouseover(function () {
-//         if( $('body').hasClass(('sidebar-expand'))){
-//             $('body').removeClass('sidebar-expand');
+//         if( $('body').hasClass(('sidebar_expand'))){
+//             $('body').removeClass('sidebar_expand');
 //             // $('.left_nav').css('positon','absolute');
 //         }
 //     })
 //     $(".left_nav").mouseleave(function () {
-//         $('body').addClass('sidebar-expand');
+//         $('body').addClass('sidebar_expand');
 //         $('.left_nav').css('positon','absolute;');
 //     })
 // });
@@ -115,12 +115,6 @@ function changeHeadrBg(color) {
         $('body').removeClass('headerColorBg5');
         $('body').removeClass('headerColorBg6');
 
-        // $('body').removeClass('changeBgActivee2');
-        // $('body').removeClass('changeBgActivee3');
-        // $('body').removeClass('changeBgActivee4');
-        // $('body').removeClass('changeBgActivee5');
-        // $('body').removeClass('changeBgActivee6');
-
     }
     if (color == '#fe5419') {
         $('body').addClass('headerColorBg2 ');
@@ -129,12 +123,6 @@ function changeHeadrBg(color) {
         $('body').removeClass('headerColorBg4');
         $('body').removeClass('headerColorBg5');
         $('body').removeClass('headerColorBg6');
-
-        // $('body').removeClass('changeBgActivee1');
-        // $('body').removeClass('changeBgActivee3');
-        // $('body').removeClass('changeBgActivee4');
-        // $('body').removeClass('changeBgActivee5');
-        // $('body').removeClass('changeBgActivee6');
     }
     if (color == '#00b0ff') {
         $('body').addClass('headerColorBg3 ');
@@ -143,12 +131,6 @@ function changeHeadrBg(color) {
         $('body').removeClass('headerColorBg4');
         $('body').removeClass('headerColorBg5');
         $('body').removeClass('headerColorBg6');
-
-        // $('body').removeClass('changeBgActivee1');
-        // $('body').removeClass('changeBgActivee2');
-        // $('body').removeClass('changeBgActivee4');
-        // $('body').removeClass('changeBgActivee5');
-        // $('body').removeClass('changeBgActivee6');
     }
     if (color == '#6659f7') {
         $('body').addClass('headerColorBg4 ');
@@ -157,12 +139,6 @@ function changeHeadrBg(color) {
         $('body').removeClass('headerColorBg3');
         $('body').removeClass('headerColorBg5');
         $('body').removeClass('headerColorBg6');
-
-        // $('body').removeClass('changeBgActivee1');
-        // $('body').removeClass('changeBgActivee2');
-        // $('body').removeClass('changeBgActivee3');
-        // $('body').removeClass('changeBgActivee5');
-        // $('body').removeClass('changeBgActivee6');
     }
     if (color == '#1d2e42') {
         $('body').addClass('headerColorBg5 ');
@@ -171,12 +147,6 @@ function changeHeadrBg(color) {
         $('body').removeClass('headerColorBg3');
         $('body').removeClass('headerColorBg4');
         $('body').removeClass('headerColorBg6');
-
-        // $('body').removeClass('changeBgActivee1');
-        // $('body').removeClass('changeBgActivee2');
-        // $('body').removeClass('changeBgActivee3');
-        // $('body').removeClass('changeBgActivee4');
-        // $('body').removeClass('changeBgActivee6');
     }
     if (color == '#f2f6fb') {
         $('body').addClass('headerColorBg6 ');
@@ -185,12 +155,6 @@ function changeHeadrBg(color) {
         $('body').removeClass('headerColorBg3');
         $('body').removeClass('headerColorBg4');
         $('body').removeClass('headerColorBg5');
-
-        // $('body').removeClass('changeBgActivee1');
-        // $('body').removeClass('changeBgActivee2');
-        // $('body').removeClass('changeBgActivee3');
-        // $('body').removeClass('changeBgActivee4');
-        // $('body').removeClass('changeBgActivee5');
     }
 }
 
@@ -296,3 +260,11 @@ function changeLeftNavBg(color) {
         $('body').removeClass('changeBgActivee5');
     }
 }
+//slide 
+var swiper = new Swiper('.swiper-container', {
+    navigation: {
+        slidesPerView: 1,
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
